09a2bf4ebd82c8304618de17d2a8442c1f5c4eae
speckworks <47194165+speckworks@users.noreply.github.com>
2024-05-09 22:06:12 -0400

    working carousel

--- my-beach-app/src/components/Carousel.tsx
+++ my-beach-app/src/components/Carousel.tsx
@@ -7,11 +7,14 @@ import { Beach } from '../interfaces/Beach'
 interface CustomSliderProps extends AwesomeSliderProps {
   onPrev: () => void;
   onNext: () => void;
+  onSelect: (index: number) => void
 }
 
 const Carousel: React.FC = () => {
   const [images, setImages] = useState<string[]>([]);
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [selected, setSelected] = useState(0);
+
 
   useEffect(() => {
     // Fetch image URLs from an API or other data source
@@ -19,9 +22,10 @@ const Carousel: React.FC = () => {
       .then((data: Beach[]) => {
         // Extract the image URLs or paths from the fetched data
         const imageUrls = data.map((item) => item.Photo_1);
-        console.log('imageUrls', imageUrls)
+        const imageUrlsFiltered = imageUrls.filter((item) => item !== "");
+        console.log('imageUrlsFiltered', imageUrlsFiltered)
         // Update the images state with the extracted image URLs or paths
-        setImages(imageUrls);
+        setImages(imageUrlsFiltered);
       })
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
@@ -34,23 +38,34 @@ const Carousel: React.FC = () => {
     setCurrentSlide((prevSlide) => (prevSlide === images.length - 1 ? 0 : prevSlide + 1));
   };
 
+  const handleSelect = (index: number) => {
+    setSelected(index);
+  };
+
   const sliderProps: CustomSliderProps = {
-    startupScreen: <div key={currentSlide} />,
+    startupScreen: <div key={ currentSlide } />,
     onPrev: handlePrevious,
     onNext: handleNext,
+    selected: currentSlide,
+    onSelect: handleSelect,
+    buttons: true,
   };
 
   return (
     <div>
-    <AwesomeSlider
-      {...sliderProps}
-    >
-      {images.map((image, index) => (
-        <div key={index} data-src={image} />
-      ))}    
-    </AwesomeSlider>
-    <button onClick={handlePrevious}>Previous</button>
-    <button onClick={handleNext}>Next</button>
+      <div className='slider-container'>
+        <AwesomeSlider
+          {...sliderProps}
+        >
+          {images.map((image, index) => (
+            <div key={index} data-src={image} />
+          ))}    
+        </AwesomeSlider>
+      </div>
+    <div className={'glass-panel'}>
+    <button className={'glass-button'}onClick={handlePrevious}>Previous</button>
+    <button className={'glass-button'} onClick={handleNext}>Next</button>
+    </div>
     </div>
   );
 };
