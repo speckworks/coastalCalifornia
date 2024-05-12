3785b11fe3d2105d1c8bdae6ceac4c7c01e9ea79
speckworks <47194165+speckworks@users.noreply.github.com>
2024-05-12 10:49:59 -0400

    slider working, next button requires doubleclick

--- my-beach-app/src/components/Carousel.tsx
+++ my-beach-app/src/components/Carousel.tsx
@@ -7,13 +7,11 @@ import { Beach } from '../interfaces/Beach'
 interface CustomSliderProps extends AwesomeSliderProps {
   onPrev: () => void;
   onNext: () => void;
-  onSelect: (index: number) => void
 }
 
 const Carousel: React.FC = () => {
   const [images, setImages] = useState<string[]>([]);
   const [currentSlide, setCurrentSlide] = useState(0);
-  const [selected, setSelected] = useState(0);
 
 
   useEffect(() => {
@@ -23,7 +21,6 @@ const Carousel: React.FC = () => {
         // Extract the image URLs or paths from the fetched data
         const imageUrls = data.map((item) => item.Photo_1);
         const imageUrlsFiltered = imageUrls.filter((item) => item !== "");
-        console.log('imageUrlsFiltered', imageUrlsFiltered)
         // Update the images state with the extracted image URLs or paths
         setImages(imageUrlsFiltered);
       })
@@ -38,16 +35,10 @@ const Carousel: React.FC = () => {
     setCurrentSlide((prevSlide) => (prevSlide === images.length - 1 ? 0 : prevSlide + 1));
   };
 
-  const handleSelect = (index: number) => {
-    setSelected(index);
-  };
 
   const sliderProps: CustomSliderProps = {
-    startupScreen: <div key={ currentSlide } />,
     onPrev: handlePrevious,
     onNext: handleNext,
-    selected: currentSlide,
-    onSelect: handleSelect,
     buttons: true,
   };
 
